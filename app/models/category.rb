d53f916dfa9fbbd063fefc9f89564b253c397d55
Pablo De Los Santos Fernández <pablodls96@gmail.com>
2016-09-24 14:53:55 -0500

    add dynamic scopes in product_type and catogry models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,10 @@
+#crear de forma dinamica metodos
 class Category < ActiveRecord::Base
+  uniq.map{|c| c.name}.each do |v| 
+    self.class.class_eval do
+      define_method v.downcase do 
+        find_by_name(v)
+      end
+    end
+  end
 end
